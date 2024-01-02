db7f6b021a70fb0df7c30bf2a89652d3db726d14
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2024-01-01 23:13:34 -0600

    01/01/2024

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -20,6 +20,7 @@ export class PerfilPage implements OnInit {
   // eslint-disable-next-line max-len
   pattern: any = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   date = "";
+  momento = "";
 
   constructor(private userService: UserService, private storage: Storage, private alertService: AlertService,
               private navCtrl: NavController, private loadingController: LoadingController) {
@@ -38,14 +39,20 @@ export class PerfilPage implements OnInit {
     return new FormGroup({
         dpi: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/)]),
         nombre: new FormControl('', [Validators.required]),
+        nombrejudio: new FormControl('', [Validators.required]),
         apellido: new FormControl('', [Validators.required]),
         telefono: new FormControl('', [Validators.required, Validators.pattern(/^\d{8}$/)]),
+        parasha: new FormControl('', [Validators.required]),
         celular: new FormControl('', [Validators.required, Validators.pattern(/^\d{8}$/)]),
         mail: new FormControl('', [Validators.required, Validators.pattern(this.pattern)]),
         direccion: new FormControl('', [Validators.required]),
-        trabajo: new FormControl('', [Validators.required]),
-        telTra: new FormControl('', [Validators.required, Validators.pattern(/^\d{8}$/)]),
-        profesion: new FormControl('', [Validators.required])
+        departamento: new FormControl('', [Validators.required]),
+        municipio: new FormControl('', [Validators.required]),
+        edad: new FormControl('', [Validators.required, Validators.pattern(/^\d{2}$/)]),
+        genero: new FormControl('', [Validators.required]),
+        nacionalidad: new FormControl('', [Validators.required]),
+        tipoSangre: new FormControl('', [Validators.required]),
+        estado: new FormControl('', [Validators.required])
     });
 }
 
@@ -61,33 +68,49 @@ onlyNumbers(event: KeyboardEvent) {
     console.log(perfilData)
     this.profileForm.controls['dpi'].setValue(perfilData.cui);
     this.profileForm.controls['nombre'].setValue(perfilData.nombre);
+    this.profileForm.controls['nombrejudio'].setValue(perfilData.nombrejudio);
     this.profileForm.controls['apellido'].setValue(perfilData.apellido);
     this.profileForm.controls['telefono'].setValue(perfilData.telefono);
+    this.profileForm.controls['parasha'].setValue(perfilData.parasha);
     this.profileForm.controls['celular'].setValue(perfilData.celular);
     this.profileForm.controls['mail'].setValue(perfilData.mail);
     this.profileForm.controls['direccion'].setValue(perfilData.direccion);
-    this.profileForm.controls['trabajo'].setValue(perfilData.nacionalidad);
-    this.profileForm.controls['telTra'].setValue(perfilData.telefono_trabajo);
-    this.profileForm.controls['profesion'].setValue(perfilData.profesion);
+    this.profileForm.controls['departamento'].setValue(perfilData.departamento);
+    this.profileForm.controls['municipio'].setValue(perfilData.municipio);
+    this.profileForm.controls['edad'].setValue(perfilData.edad);
+    this.profileForm.controls['genero'].setValue(perfilData.genero);
+    this.profileForm.controls['nacionalidad'].setValue(perfilData.nacionalidad);
+    this.profileForm.controls['tipoSangre'].setValue(perfilData.tipoSangre);
+    this.profileForm.controls['estado'].setValue(perfilData.estado);
   }
 
   get dpi() { return this.profileForm.get('dpi'); }
   get nombre() { return this.profileForm.get('nombre'); }
+  get nombrejudio() { return this.profileForm.get('nombrejudio'); }
   get apellido() { return this.profileForm.get('apellido'); }
   get telefono() { return this.profileForm.get('telefono'); }
+  get parasha() { return this.profileForm.get('parasha'); }
   get celular() { return this.profileForm.get('celular'); }
   get mail() { return this.profileForm.get('mail'); }
   get direccion() { return this.profileForm.get('direccion'); }
-  get trabajo() { return this.profileForm.get('trabajo'); }
-  get telTra() { return this.profileForm.get('telTra'); }
-  get profesion() { return this.profileForm.get('profesion'); }
+  get departamento() { return this.profileForm.get('departamento'); }
+  get municipio() { return this.profileForm.get('municipio'); }
+  get edad() { return this.profileForm.get('edad'); }
+  get genero() { return this.profileForm.get('genero'); }
+  get nacionalidad() { return this.profileForm.get('nacionalidad'); }
+  get tipoSangre() { return this.profileForm.get('tipoSangre'); }
+  get estado() { return this.profileForm.get('estado'); }
 
   async getData() {
       (await this.userService.getPerfil()).subscribe((resp: any) => {
-        this.perfilData = resp[0];
-        this.defaultValue( this.perfilData );
-        this.date = resp[0].fecha_nacimiento + "T00:00:00";
-        this.mostrarData = true;
+        if(resp.status){
+          this.perfilData = resp.data[0];
+          this.defaultValue( this.perfilData );
+          this.date = resp.data[0].fecha_nacimiento + "T00:00:00";
+          this.mostrarData = true;
+        }else{
+          this.alertService.presentToast(resp.message, 'danger', 3000);
+        }
       });
   }
 
@@ -113,39 +136,11 @@ onlyNumbers(event: KeyboardEvent) {
 
   async updatePerfil(){
     await this.presentLoading();
-    (await this.userService.update(this.profileForm.value.dpi, 'cui', this.profileForm.value.dpi)).subscribe(async resp =>{
+    (await this.userService.updateFamilyMemberProfile(this.profileForm.value.dpi, 'DPI', this.profileForm.value.nombre, this.profileForm.value.apellido, this.profileForm.value.nombrejudio, this.date, this.date, this.momento, this.date, "", this.profileForm.value.estado, this.profileForm.value.nacionalidad, this.profileForm.value.telefono, this.profileForm.value.celular, this.profileForm.value.email, this.profileForm.value.direccion, this.profileForm.value.departamento, this.profileForm.value.municipio, '', '', '', this.profileForm.value.genero, this.profileForm.value.tipoSangre, '', '', '', this.profileForm.value.parasha )).subscribe(async resp =>{
       console.log(resp);
+      await this.loadingController.dismiss();
+      this.navCtrl.back();
     });
-    (await this.userService.update(this.profileForm.value.dpi, 'nombre', this.profileForm.value.nombres)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'apellido', this.profileForm.value.apellido)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'telefono', this.profileForm.value.telefono)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'celular', this.profileForm.value.celular)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'direccion', this.profileForm.value.direccion)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'fecha_nacimiento', this.date)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'lugar_trabajo', this.profileForm.value.trabajo)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'telefono_trabajo', this.profileForm.value.telTra)).subscribe(resp =>{
-      console.log(resp)
-    });
-    (await this.userService.update(this.profileForm.value.dpi, 'profesion', this.profileForm.value.profesion)).subscribe(resp =>{
-      console.log(resp)
-    });
-    await this.loadingController.dismiss();
-    this.navCtrl.back();
-  
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -50,8 +50,10 @@ export class UserService {
     return this.http.get<T>(`${asmsURL}API_perfil_padre.php?request=registro&dpi=${dpi}&email=${email}&nombre=${nombre}&apellido=${apellido}`);
   }
 
-  async update<T>(dpi: any, campo: any, valor: any){
+  async updateFamilyMemberProfile(cui: string, tipoCui: string, nombre: string, apellido: string, nombreJudio: string, fechaNac: string, fechaJudia: string, momentoDia: string, barMitzva: string, fechaFallecimiento: string, estadoCivil: string, nacionalidad: string, telCasa: string, celular: string, email: string, direccion: string, departamento: string, municipio: string, trabajo: string, telTrabajo: string, profesion: string, genero: string, sangre: string, alergia: string, emergencia: string, emeTel: string, parasha: string) {
     this.datosUsuario = await this.storage.get('datos');
-    return this.http.get<T>(`${asmsURL}API_perfil_padre.php?request=update_padre&usuario=${this.datosUsuario.codigo}&cui=${dpi}&campo=${campo}&valor=${valor}`);
+    console.log(`${asmsURL}API_perfil_padre.php?request=update_padre_completo&codigo_unico=${this.datosUsuario.codigo}&cui=${cui}&tipocui=${tipoCui}&nombre=${nombre}&apellido=${apellido}&nombreJudio=${nombreJudio}&fecnac=${fechaNac}&fechaJudia=${fechaJudia}&momentoDia=${momentoDia}&barMitzva=${barMitzva}&fechaFallecimiento=${fechaFallecimiento}&estadoCivil=${estadoCivil}&nacionalidad=${nacionalidad}&telcasa=${telCasa}&celular=${celular}&mail=${email}&direccion=${direccion}&departamento=${departamento}&municipio=${municipio}&trabajo=${trabajo}&teltrabajo=${telTrabajo}&profesion=${profesion}&genero=${genero}&sangre=${sangre}&alergia=${alergia}&emergencia=${emergencia}&emetel=${emeTel}&parasha=${parasha}`)
+    return this.http.get(`${asmsURL}API_perfil_padre.php?request=update_padre_completo&codigo_unico=${this.datosUsuario.codigo}&cui=${cui}&tipocui=${tipoCui}&nombre=${nombre}&apellido=${apellido}&nombreJudio=${nombreJudio}&fecnac=${fechaNac}&fechaJudia=${fechaJudia}&momentoDia=${momentoDia}&barMitzva=${barMitzva}&fechaFallecimiento=${fechaFallecimiento}&estadoCivil=${estadoCivil}&nacionalidad=${nacionalidad}&telcasa=${telCasa}&celular=${celular}&mail=${email}&direccion=${direccion}&departamento=${departamento}&municipio=${municipio}&trabajo=${trabajo}&teltrabajo=${telTrabajo}&profesion=${profesion}&genero=${genero}&sangre=${sangre}&alergia=${alergia}&emergencia=${emergencia}&emetel=${emeTel}&parasha=${parasha}`);
   }
+
 }
